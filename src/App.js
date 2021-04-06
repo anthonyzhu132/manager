95b13e0e0c02d383f361a72ff193e79e6fb6c967
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-05 21:03:50 -0700

    added input fields for login form g

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers/index';
 import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -23,11 +23,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Card from './common/Card';
+import CardItem from './common/CardItem';
+import Input from './common/Input';
+import Button from './common/Button';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
-
+        <CardItem>
+          <Input label="Email" placeholder="email@gmail.com"/>
+        </CardItem>
+        <CardItem>
+          <Input secureTextEntry label="password" placeholder="password"/>
+        </CardItem>
+        <CardItem>
+          <Button>
+            Login
+          </Button>
+        </CardItem>
       </Card>
     )
   };
