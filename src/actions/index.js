c250ca04f5510b51970e29ad8364b84de344f768
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-12 21:42:28 -0700

    added fail case

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -18,12 +18,19 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password}) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(user => loginUserSuccess(dispatch, user));
-    .catch(() => {
-      firebase.auth.createUserWithEmailAndPassword(email, password)
-        .then((user) => loginUserSuccess(dispatch, user));
-    });
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth.createUserWithEmailAndPassword(email, password)
+          .then((user) => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
   };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
 };
 
 const loginUserSuccess = (dispatch, user) => {
