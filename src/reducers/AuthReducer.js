08dfcd36cad0bec50f9481e8e8aa6723b81d8771
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-13 23:39:20 -0700

    added loading state to load spinner

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,6 +1,6 @@
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/types';
 
-const INITIAL_STATE = { email: '', password: '', user: null, error: '' }
+const INITIAL_STATE = { email: '', password: '', user: null, error: '', loading: false }
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
@@ -12,6 +12,8 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, user: action.payload, error: '' };
     case LOGIN_USER_FAIL:
       return { ...state, error: 'Authentication Failed' };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' };
     default:
       return state;
   };
