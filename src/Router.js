a100d3a0b7de36fb500260d11ae69746f97273fb
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-14 21:50:46 -0700

    added LoginForm to router and to show Router to decide what to load first on App.js

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,11 @@ import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
   return (
-
+    <Router>
+      <Scene key="root">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+    </Router>
   );
 };
 
