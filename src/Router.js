dfd97fc08844a2436e3289b4a6695b3563c047ca
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-14 21:57:25 -0700

    added EmployeeList page for navigation

--- src/Router.js
+++ src/Router.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 
+
 const RouterComponent = () => {
   return (
-    <Router>
+    <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key="root">
         <Scene key="login" component={LoginForm} title="Please Login" />
       </Scene>
