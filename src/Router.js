4e3a7ac36ab9831f9249157c42fb9569c98ac41e
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-14 21:47:58 -0700

    added react-native-router-flux for navigation as well as Router.js boilerplate

---
+++ src/Router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => {
+  return (
+
+  );
+};
+
+export default RouterComponent;
