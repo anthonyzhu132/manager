e9d2054191484743eda606d8e635dcc1525711f6
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-16 22:51:31 -0700

    added inital state

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,6 @@
 import { EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { name: '', phone: '', shift: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
