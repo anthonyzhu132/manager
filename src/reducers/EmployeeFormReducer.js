f5a73d228489fc8f2b3e4c025bb9b53c46ec4642
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-16 22:50:10 -0700

    added case for Employee Update

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -4,8 +4,10 @@ const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      //action.payload === { prop: 'name', value: 'Tom'}
+      return { ...state, [action.payload.prop]: action.payload.value }
     default:
       return state;
   }
 };
-
