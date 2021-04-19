bccc87081a765db00858da49f4a3d2394089d9d3
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-18 17:22:21 -0700

    added router-flux onto Employee create to satisfy error of needing an action, therefor using it to redirect user back to employeeList when done

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,6 @@
 import { EMPLOYEE_UPDATE } from './types'
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -12,7 +13,10 @@ export const employeeCreate = ({ name, phone, shift}) => {
   //Grabs current user from firebase.auth
   const { currentUser } = firebase.auth();
 
-  //Access current users employee path and push the information onto corresponding user
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  return () => {
+    //Access current users employee path and push the information onto corresponding user
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => Actions.employeeList())
+  };
 };
