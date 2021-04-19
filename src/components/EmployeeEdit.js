2b1378c224cdf403148a50d1fd852951c573b6dd
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-19 13:51:31 -0700

    added boilerplate for EmployeeEdit page

---
+++ src/components/EmployeeEdit.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { employeeUpdate, employeeCreate } from '../actions';
+import React, { Component } from 'react';
+import Card from './common/Card';
+import CardItem from './common/CardItem';
+import Button from './common/Button';
+import EmployeeForm from './EmployeeForm';
+
+class EmployeeEdit extends Component {
+  render() {
+    return (
+      <Card>
+        <CardItem>
+          <Button>
+            Save Changes
+          </Button>
+        </CardItem>
+      </Card>
+    )
+  }
+}
+
+export default EmployeeEdit;
