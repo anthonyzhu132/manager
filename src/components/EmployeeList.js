7a5bbafe68f5f946bb1da5703863e844a000b3c4
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-18 17:56:59 -0700

    added helper function to help create ListView with the fetched information from this.props.EmployeesFetch();

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import ListView from 'deprecated-react-native-listview';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.DataSource = ds.cloneWithRwos(employees);
+  }
+
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  };
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are new props that are going to be used to render with
+    // this.props are still old props before having information
+    this.createDataSource(nextProps);
   }
 
   render() {
--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -1,11 +1,11 @@
 import { EMPLOYEES_FETCH_SUCCESS } from '../actions/types';
 
-const INITIAL_STATE = { };
+const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case EMPLOYEES_FETCH_SUCCESS:
-      return state;
+      return action.payload;
     default:
       return state;
   }
