5afa5c82507b38fcdefd98a088a52cc66c069a41
anthonyzhu132 <anthonyzhu132@gmail.com>
2021-04-20 12:30:46 -0700

    added buttons and ability to accept children from props object

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -3,19 +3,19 @@ import { Text, View, Modal } from 'react-native';
 import CardItem from './CardItem';
 import Button from './Button';
 
-const Confirm = () => {
+const Confirm = ({ children }) => {
   return (
     <Modal>
       <View>
         <CardItem>
           <Text>
-
+            {children}
           </Text>
         </CardItem>
 
         <CardItem>
-          <Button>Yes</Button>
-          <Button>No</Button>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
         </CardItem>
       </View>
     </Modal>
